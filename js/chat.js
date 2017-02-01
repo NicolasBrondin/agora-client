7205a0aee88ac25fe7c71c3954861fb74b102fb7
NicolasBrondin <nicolas@brondin.com>
2017-02-01 06:19:14 +0100

    [DESIGN] Added a logo and some design tweaks

---
+++ js/chat.js
@@ -0,0 +1,33 @@
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {}, tokens, re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+      
+var query = getQueryParams(document.location.search);
+$('#chat_header').html('Room: '+query.room);
+var socket = io('https://agora-chat-server.herokuapp.com/');
+
+$('#send_message').click(function(){
+    socket.emit('chat message', {username: query.username, room: query.room, msg:$('#user_message').val()});
+    $('#user_message').val('');
+    return false;
+});
+
+socket.on('chat message', function(msg){
+    $('#message-list').append($('<li>').append($('<p>').text(msg.username+": "+msg.msg)));
+});
+        
+socket.on('setup', function(msg){
+    socket.emit('setup', {room: query.room, username: query.username});
+});
+        
+socket.on('user joined', function(msg){
+    $('#message-list').append($('<li>').append($('<p>').text("User joined: "+msg.username)));
+});
---
+++ js/index.js
@@ -0,0 +1,45 @@
+      
+        function update_rooms(lat, long){
+            $('#location_button').html("Fetching rooms !"); 
+            $.ajax({
+                url: "https://agora-chat-server.herokuapp.com/rooms/lat/"+lat+"/long/"+long
+            }).done(function( data ) {               
+                console.log(data);
+                $("#rooms").html("");
+                 
+                
+                data.rooms.forEach(function(room){
+                    var o = new Option(room.name, room.name);
+                    $(o).html(room.name);
+                    $("#rooms").append(o);
+                });
+                $('#location_button').html("Rooms found !"); 
+                $('#username_group').removeClass('hide');
+                $('#room_group').removeClass('hide');
+                $('#send').removeClass('disabled');
+                $('#rooms').val(data.rooms[0].name);
+            }).fail(function(){
+                 $('#location_button').html("Location error..."); 
+            });
+        }
+        
+        $('#send').click(function(){
+            
+            window.location = "chat.html?room="+$('#rooms').val()+"&username="+$('#username').val();
+            console.log("lol");
+        });
+        
+        $('#location_button').click(function(){
+            $('#location_button').html("Fetching location...");
+            if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                update_rooms(position.coords.latitude, position.coords.longitude);
+            });
+            var watchID =  navigator.geolocation.watchPosition(function(position) {
+            update_rooms(position.coords.latitude, position.coords.longitude);
+                navigator.geolocation.clearWatch(watchID);
+            });
+        } else {
+          alert("Geolocation not available");
+        }
+        });
