e603639a27550d17f1315ad76711fe13b56ea237
NicolasBrondin <nicolas@brondin.com>
2017-02-01 07:23:26 +0100

    [REFACTOR] Added host variable for local tests

--- js/chat.js
+++ js/chat.js
@@ -1,3 +1,6 @@
+var host = 'http://localhost:3000';
+//var host = 'https://agora-chat-server.herokuapp.com';
+
 function getQueryParams(qs) {
     qs = qs.split('+').join(' ');
 
@@ -12,7 +15,7 @@ function getQueryParams(qs) {
       
 var query = getQueryParams(document.location.search);
 $('#chat_header').html('Room: '+query.room);
-var socket = io('https://agora-chat-server.herokuapp.com/');
+var socket = io(host);
 
 $('#send_message').click(function(){
     socket.emit('chat message', {username: query.username, room: query.room, msg:$('#user_message').val()});
